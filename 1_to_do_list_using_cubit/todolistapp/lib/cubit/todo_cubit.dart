68550fa944a1b56af1456d3661354cbd4213fe0b
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2024-07-12 11:31:29 +0530

    cubit has onerror method by this method we can show the error

--- 1_to_do_list_using_cubit/todolistapp/lib/cubit/todo_cubit.dart
+++ 1_to_do_list_using_cubit/todolistapp/lib/cubit/todo_cubit.dart
@@ -2,14 +2,33 @@
 
 
 
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todolistapp/model/todo_model.dart';
 
 class TodoCubit extends Cubit<List<Todo>>{
+
+
   TodoCubit() : super([]);
+
+
   void addTodo({required Todo task}){
+    if(task.taskName.isEmpty){
+      addError("Hay Tasktitle is not Empty",);  
+    }
     emit([...state,task]);
+
   }
 
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {  //this is the method of cubit and uesed to show or print the error by addEroor like inside the addTodo method method
+
+    // TODO: implement onError
+    super.onError(error, stackTrace);
+    log("$error");
+  }
   
 }
